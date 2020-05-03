31982f13b44bfcebfb6c0585f7675484c25690d7
paullawrence <Lawrence_pau@hotmail.co.uk>
2020-05-03 20:49:05 +0100

    sTART character development

---
+++ PlayerCharacter/PlayerCharacterGame.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerCharacter
+{
+    internal class PlayerCharacterGame : Game
+    {
+        GraphicsDeviceManager _graphicsDevice;
+        SpriteBatch _spriteBatch;
+        SpriteFont arial;
+        public PlayerCharacterGame()
+        {
+            _graphicsDevice = new GraphicsDeviceManager(this);
+            _graphicsDevice.PreferredBackBufferWidth = 800;
+            _graphicsDevice.PreferredBackBufferHeight = 800;
+        }
+
+        protected override void Initialize()
+        {
+            _spriteBatch = new SpriteBatch(_graphicsDevice.GraphicsDevice);
+        }
+
+        protected override void LoadContent()
+        {
+            this.Content.RootDirectory = "./Content";
+
+            this.arial = this.Content.Load<SpriteFont>("NewArial");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _graphicsDevice.GraphicsDevice.Clear(Color.AntiqueWhite);
+            _spriteBatch.Begin();
+            _spriteBatch.End();
+        }
+    }
+}
---
+++ PlayerCharacter/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayerCharacter
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using(var game = new PlayerCharacterGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
